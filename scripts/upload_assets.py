5a2fb2b5ce5785a71253e48b543c41709f6e286b
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2023-12-05 12:56:25 +0000

    Trying to use requests lib

---
+++ scripts/upload_assets.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+def upload_asset(upload_url, token, file_path):
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    with open(file_path, 'rb') as file:
+        files = {
+            "file": (os.path.basename(file_path), file, "application/octet-stream")
+        }
+        response = requests.post(upload_url, headers=headers, files=files)
+        response.raise_for_status()
+
+def main():
+    token = os.environ['GITHUB_TOKEN']
+    upload_url = os.environ['UPLOAD_URL']
+    dist_path = 'dist'
+
+    for filename in os.listdir(dist_path):
+        file_path = os.path.join(dist_path, filename)
+        if os.path.isfile(file_path):
+            print(f"Uploading {file_path}...")
+            upload_asset(upload_url, token, file_path)
+
+if __name__ == "__main__":
+    main()
