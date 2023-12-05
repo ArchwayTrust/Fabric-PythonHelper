8a0d34fd8c76377b3d54e2ffe630949595f69349
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2023-12-05 13:13:20 +0000

    Changing upload url

--- scripts/upload_assets.py
+++ scripts/upload_assets.py
@@ -6,11 +6,15 @@ def upload_asset(upload_url, token, file_path):
         "Authorization": f"token {token}",
         "Accept": "application/vnd.github.v3+json"
     }
+
+    # Construct the actual upload URL
+    actual_url = upload_url.replace("{?name,label}", "") + f"?name={os.path.basename(file_path)}"
+
     with open(file_path, 'rb') as file:
         files = {
             "file": (os.path.basename(file_path), file, "application/octet-stream")
         }
-        response = requests.post(upload_url, headers=headers, files=files)
+        response = requests.post(actual_url, headers=headers, files=files)
         response.raise_for_status()
 
 def main():
