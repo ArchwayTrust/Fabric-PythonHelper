bee4e56e44c44511a0f6846d9e5e075f4cf44e2c
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2023-12-05 11:59:58 +0000

    First publish to pypi.

--- fabric_python_helper/__init__.py
+++ fabric_python_helper/__init__.py
@@ -3,7 +3,7 @@ from .pbi_admin import AccessTokens
 from .pbi_admin import Dataflows
 from .pbi_admin import SemanticModels
 
-__version__="0.2.7"
+__version__="0.2.8"
 __author__="Ben Dobbs"
 __author_email__="bdobbs@archwaytrust.co.uk"
 __description__="Collection of useful python code for automation in Microsoft Fabric."
